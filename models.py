5f65b097cf050d8075060c31a81a2da587860581
Sheik Mohammed Ali M <69857002+sheik-md-ali@users.noreply.github.com>
2025-03-20 18:41:41 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(512), nullable=False)  # Increase the size of the password column
+    password = db.Column(db.String(512), nullable=False)  
 
     def __repr__(self):
         return f'<User {self.username}>'
